850558d78a03ec949cb32056ec7cf757d6eb4fc5
Hossein2468 <hosseinbaziepic@gmail.com>
2022-07-17 19:54:35 +0430

    added P005

---
+++ P005/P005-operator.py
@@ -0,0 +1,11 @@
+a = int(input("enter the first number: "))
+b = input("enter the operator: ")
+c = int(input("enter the second number:"))
+if b == "+":
+    print (a + c)
+elif b == "-":
+    print (a - c)
+elif b == "*":
+    print (a * c)
+elif b == "/":
+    print (a / c)
